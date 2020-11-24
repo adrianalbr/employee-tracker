38c0488425d69b25ccf553621f71551147515c50
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2020-11-23 19:57:59 -0500

    debugged view functions and started on add function

--- employeeTracker.js
+++ employeeTracker.js
@@ -11,7 +11,6 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
-  
   init();
 });
 
@@ -20,9 +19,11 @@ function init() {
     .prompt([
       {
         type: "list",
-        message: "Welcome to the Ministry of Magic Employee Tracker! What you like to do?",
+        message:
+          "Welcome to the Ministry of Magic Employee Tracker! What you like to do?",
         name: "search",
         choices: [
+          "test",
           "View all employees",
           "View all employees by Department",
           "View all employees by Manager",
@@ -30,96 +31,196 @@ function init() {
           "Remove and employee",
           "Update employee role",
           "Update employee manager",
-          "Exit",
+          "exit",
         ],
       },
     ])
     .then(({ search }) => {
       switch (search) {
+        case "test":
+          test();
+          break;
         case "View all employees":
           allEmployees();
           break;
-        case "View employees by department":
-          employeesBydepartment();
+        case "View all employees by Department":
+          byDepartment();
           break;
-        case "View employees by Manager":
-            employeesBymanager();
+        case "View all employees by Manager":
+          byManager();
           break;
-        case "Exit":
+          case "Add a new employee":
+          addEmployee();
+          break;
+        case "exit":
           exit();
           break;
         default:
           console.log("You have entered an invalid command, please try again");
+          init();
       }
     });
 }
 
 //FUNCTIONS
+
+//test
+function test() {
+  connection.query("SELECT * from department;", function (err, data) {
+    if (err) throw err;
+    console.table(data);
+    init();
+  });
+}
+
 //View all employees
 
 function allEmployees() {
-      connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, e2.first_name AS manager FROM employee LEFT JOIN employee as e2 ON e2.id = employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id",
-        (err, data) => {
-          if (err) throw err;
-          console.table(data);
-          init();
-        }
-      );
-    };
-
-function employeesBydepartment() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What Department would you like to see?",
-        name: "title",
-      },
-    ])
-    .then(({ department }) => {
-      connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, e2.first_name AS manager FROM employee LEFT JOIN employee as e2 ON e2.id = employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id WHERE department.name = ? ORDER BY employee.id;"
-        [department],
-        (err, data) => {
-          if (err) throw err;
-          console.table(data);
-          init();
-        }
-      );
-    });
+  connection.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, e2.first_name AS manager FROM employee LEFT JOIN employee as e2 ON e2.id = employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id",
+    function (err, data) {
+      if (err) throw err;
+      console.table(data);
+      init();
+    }
+  );
 }
 
-function employeesBymanager() {
-  // All distinct managers from employee table
-  connection.query("SELECT DISTINCT e2.first_name, e2.last_name FROM employee LEFT JOIN employee AS e2 ON employee.manager_id = e2.id WHERE e2.first_name IS NOT NULL",
-function (err, results) {
+function byDepartment() {
+  //show all available departments
+  connection.query("SELECT * FROM department", function (err, results) {
     if (err) throw err;
     inquirer
-        .prompt([
-            {
-                name: "manager",
-                type: "list",
-                choices: function () {
-                    let choiceArray = [];
-                    for (var i = 0; i < results.length; i++) {
-                        choiceArray.push(results[i].first_name);
-                    }
-                    return choiceArray;
-                },
-                message: "Which magical manager would you like to search by?"
+      .prompt([
+        {
+          type: "list",
+          name: "department",
+          choices: function () {
+            let choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].name);
             }
+            return choiceArray;
+          },
+          message: "What department would you like to search by",
+        },
+      ])
+      .then(({ department }) => {
+        console.log(department);
+        connection.query(
+          `
+        SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, e2.first_name AS manager
+        FROM employee
+        LEFT JOIN employee as e2 ON e2.id = employee.manager_id
+        JOIN role ON employee.role_id = role.id
+        JOIN department ON role.department_id = department.id
+        WHERE department.name = ?
+        ORDER BY employee.id;
+        `,
+          [department],
+          (err, data) => {
+            if (err) throw err;
+            console.table(data);
+            init();
+          }
+        );
+      });
+  });
+}
+
+function byManager() {
+  // All distinct managers from employee table
+  connection.query(
+    "SELECT DISTINCT e2.first_name, e2.last_name FROM employee LEFT JOIN employee AS e2 ON employee.manager_id = e2.id WHERE e2.first_name IS NOT NULL",
+    function (err, results) {
+      if (err) throw err;
+      inquirer
+        .prompt([
+          {
+            name: "manager",
+            type: "list",
+            choices: function () {
+              let choiceArray = [];
+              for (var i = 0; i < results.length; i++) {
+                choiceArray.push(results[i].first_name);
+              }
+              return choiceArray;
+            },
+            message: "Which magical manager would you like to search by?",
+          },
         ])
         .then(function (answer) {
-            console.log(answer.manager);
-            let query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, e2.first_name AS manager FROM employee LEFT JOIN employee AS e2 ON e2.id = employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id WHERE e2.first_name = ? ORDER BY employee.id;'
-            connection.query(query, answer.manager, function (err, res) {
-                if (err) throw err;
-                console.table(res);
-                init();
-            });
+          console.log(answer.manager);
+          let query =
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, e2.first_name AS manager FROM employee LEFT JOIN employee AS e2 ON e2.id = employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id WHERE e2.first_name = ? ORDER BY employee.id;";
+          connection.query(query, answer.manager, function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            init();
+          });
         });
-});
+    }
+  );
+};
+
+function addEmployee(){
+  let newEmployee = {};
+  connection.query("SELECT * FROM role", function (err, results){
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "first_name",
+          type: "input",
+          message: "What is the employee's first name?",
+          validate: function (answer) {
+            if (answer.length <1) {
+              return console.log("valid name required");
+            }
+            return true;
+          }
+
+        },
+
+        {
+          name: "last_name",
+          type: "input",
+          message: "What is the employee's last name?",
+          validate: function (answer) {
+            if (answer.length <1) {
+              return console.log("valid last name required");
+            }
+            return true;
+          }
+        },
+
+        {
+          name: "role",
+          type: "list",
+          choices: function () {
+            let choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].title);
+            }
+            return choiceArray;
+          },
+          message: "What is the employee's role?",
+        }
+      ])
+      .then(function (answer){
+        newEmployee.first_name = answer.first_name;
+        newEmployee.last_name = answer.last_name;
+
+        //role to role id
+        
+
+
+      })
+
+
+  }
+  
+  )
 }
 
 
